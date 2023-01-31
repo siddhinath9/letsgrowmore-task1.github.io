5abdd86a50f8df8999aab9288a6f7a2499049ba3
siddhinath9 <107683191+siddhinath9@users.noreply.github.com>
2023-01-31 20:05:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+document.querySelector('#submit').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("You need to enter a task!!")
+    }
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+    }
+}
